75ac78d376d4df0b838f22be6511d8f68abe93da
DmitrySerovd <d.serov@sh.dunice.net>
2024-06-05 12:03:41 +0300

    Created first version

---
+++ Article-titles/ArticleProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Article_titles
+{
+    public class ArticleProvider
+    {
+        public static string GetTitle(string[] titleWords, string[] exceptionWords)
+        {
+            var textInfo = new CultureInfo("ru-RU").TextInfo;
+            titleWords[0] = textInfo.ToTitleCase(textInfo.ToLower(titleWords[0]));
+            for (int i = 1; i < titleWords.Length; i++)
+            {
+                if (exceptionWords.Contains(titleWords[i].ToLower()) == false)
+                {
+                    titleWords[i] = textInfo.ToTitleCase(textInfo.ToLower(titleWords[i]));
+                }
+                else
+                {
+                    titleWords[i] = titleWords[i].ToLower();
+                }
+            }
+            return string.Join(" ", titleWords);
+        }
+    }
+}
---
+++ Article-titles/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Article_titles;
+
+string text = "the CLASH OF clans";
+string exceptionWords = "the of";
+Console.WriteLine("Начальные данные: " + text +' '+exceptionWords);
+Console.WriteLine(ArticleProvider.GetTitle(text.Split(' '), exceptionWords.Split(' ')));
